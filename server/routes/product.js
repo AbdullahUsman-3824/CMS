8b607ea547c3216dd437177a5d8cf7421f6dc6fd
M. Abdullah Usman <abdullaasheikh3824@gmail.com>
2025-02-05 19:54:16 +0500

    Updated Mongoose Connection Setup

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,5 @@
 // Import required modules
 const express = require("express");
-const mongoose = require("mongoose");
 const router = express.Router();
 
 const Product = require("../models/product");
--- server/server.js
+++ server/server.js
@@ -3,43 +3,34 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const mongoose = require("mongoose");
+const db = require("./mongoose.js");
+db.connect();
 
 const CategoryRouter = require("./routes/category.js");
 const ProductRouter = require("./routes/product");
 
-// Use native promises for mongoose
-mongoose.Promise = global.Promise;
-
 // Middleware to parse JSON bodies
-app.use(express.json());
-
-// Connect to MongoDB at default port 27017    // need improvement in future for prodcution
-(async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/CMS");
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("> Error connecting to MongoDB:", error.message);
-    process.exit(1);
-  }
-})();
+// app.use(express.json());
 
 // Use routes
 app.use("/api/categories", CategoryRouter);
 app.use("/api/products", ProductRouter);
 
+// Catch-all route for handling 404 errors
+app.use("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
+
 // Global error handling middleware
 app.use((err, req, res, next) => {
   const { status = 500, message = "Something went wrong" } = err;
-  console.log("ERROR!");
   res.status(status).send(message);
 });
-
-app.use("*", (req, res) => res.send("Page not found"));
 
 // Root route
 app.get("/", (req, res) => res.send("Hello World!"));
 
 // Start the server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
